5808a9095e71eac76dcc0ce56202d042eab29000
sohelpatel1908 <patelsohel1908@gmail.com>
2026-03-23 20:29:13 +0530

    Added movies.py and movies.ipynb to notebook folder

---
+++ notebook/movies.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("D:\\CreateBot Labs Internship\\Day 2\\GitHub Assignment\\ds-git-assignment\\data\\dummy_dataset.csv")
+
+print(df.head())
